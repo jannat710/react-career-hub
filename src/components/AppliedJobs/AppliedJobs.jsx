05eadd058f7a9fe81ac796160271939d6c385b2f
e <jannatul0040@gmail.com>
2023-09-23 23:22:50 +0600

    display applied jobs

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -1,7 +1,11 @@
-
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobApplication } from "../../utility/localstorage";
 
 const AppliedJobs = () => {
     const jobs = useLoaderData();
+    const [appliedJobs, setAppliedJobs] = useState([]);
+    const [displayJobs, setDisplayJobs] = useState([]);
 
     useEffect(() => {
         const storedJobIds = getStoredJobApplication();
@@ -21,7 +25,22 @@ const AppliedJobs = () => {
     }, [jobs])
     return (
         <div>
-            <h2>Jobs I applied</h2>
+            <h2 className="text-2xl">Jobs I applied : {appliedJobs.length}</h2>
+            <details className="dropdown mb-32">
+                <summary className="m-1 btn">open or close</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <li><a>All</a></li>
+                    <li><a>Remote</a></li>
+                    <li><a>Onsite</a></li>
+                </ul>
+            </details>
+            <ul>
+                {
+                    displayJobs.map(job => <li key={job.id}>
+                        <span>{job.job_title} {job.company_name}: {job.remote_or_onsite} </span>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
